8843ad7f391cc12bc3d0b6e81d7d662e4ae6b218
Jorge Terence <jorge.terence@outlook.com>
2023-02-26 14:20:09 -0300

    unicafe v3

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -5,13 +5,15 @@ export default () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const inc = (fn, state) => () => fn(state + 1);
+
   return (
     <div>
       <h1>Give Feedback! 🗒️</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}> good</button>
-        <button onClick={() => setNeutral(neutral + 1)}> neutral</button>
-        <button onClick={() => setBad(bad + 1)}> bad</button>
+        <Button name="good" callback={inc(setGood, good)} />
+        <Button name="neutral" callback={inc(setNeutral, neutral)} />
+        <Button name="bad" callback={inc(setBad, bad)} />
       </div>
 
       <h1>Statistics</h1>
@@ -19,6 +21,12 @@ export default () => {
     </div>
   );
 };
+
+const Button = ({ name, callback, classname }) => (
+  <button className={name} onClick={callback}>
+    {name}
+  </button>
+);
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = () => good + neutral + bad;
@@ -29,13 +37,30 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <br />
-      <p>total: {total()}</p>
-      <p>average: {average()}</p>
-      <p>positive: {positive()}%</p>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="Total" value={total()} />
+          <StatisticLine text="Average" value={average()} />
+          <StatisticLine text="Positive" value={positive()} percentage />
+        </tbody>
+      </table>
     </div>
   );
 };
+
+const StatisticLine = ({ text, value, percentage }) => (
+  <tr>
+    <td>
+      <b>{text}</b>
+    </td>
+    <td>
+      <code>
+        {value}
+        {percentage && "%"}
+      </code>
+    </td>
+  </tr>
+);
